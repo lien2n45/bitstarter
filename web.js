24855068ffc18ca1b5c12de94054d980990b11ed
root <root@neil-ubuntu.(none)>
2013-07-09 17:00:52 +0200

    template2

--- web.js
+++ web.js
@@ -3,7 +3,7 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, {'Content-Type': 'text/html'});
   
 var sContent = '';
 sContent = fs.readFileSync('index.html');
