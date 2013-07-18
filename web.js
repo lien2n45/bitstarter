faabd8e6ac64e8b1559bf58abd53629a167cbab0
Neil <nsblenin@gmail.com>
2013-07-18 19:49:14 +0200

    'LTCMarkt'

--- web.js
+++ web.js
@@ -3,10 +3,9 @@
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  
-var sContent = '';
-sContent = fs.readFileSync('index.html');
-res.end(sContent);
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var sContent = '';
+	sContent = fs.readFileSync('index.html');
+	res.end(sContent);
 }).listen(process.env.PORT || 8124);
 
